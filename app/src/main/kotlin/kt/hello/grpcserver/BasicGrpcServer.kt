f0893babf22e985597a35d8bb84b0f5934a90995
kmtr <kmtr@outlook.com>
2021-06-08 21:35:56 +0900

    fix logger

--- app/src/main/kotlin/kt/hello/grpcserver/BasicGrpcServer.kt
+++ app/src/main/kotlin/kt/hello/grpcserver/BasicGrpcServer.kt
@@ -3,8 +3,12 @@ package kt.hello.grpcserver
 import io.grpc.BindableService
 import io.grpc.Server
 import io.grpc.ServerBuilder
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 abstract class BasicGrpcServer(private val service: BindableService, private val env: Env) {
+
+    private val logger: Logger = LoggerFactory.getLogger(BasicGrpcServer::class.java)
 
     private val server: Server = ServerBuilder
         .forPort(env.grpcServerPort)
--- app/src/main/kotlin/kt/hello/grpcserver/HelloWorldServer.kt
+++ app/src/main/kotlin/kt/hello/grpcserver/HelloWorldServer.kt
@@ -1,13 +1,6 @@
 package kt.hello.grpcserver
 
-import io.grpc.Server
-import io.grpc.ServerBuilder
 import kt.hello.proto.GreeterGrpcKt
-import org.koin.core.component.KoinComponent
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-
-val logger: Logger = LoggerFactory.getLogger(HelloWorldServer::class.java)
 
 interface Env {
     val grpcServerPort: Int
